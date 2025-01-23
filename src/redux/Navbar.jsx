27465d24d3d5dcdfc635a57a4a50d0363763fa5e
abderrahman <arid07abderrahman@gmail.com>
2025-01-23 23:02:07 +0100

    modification de cette projet

--- src/redux/Navbar.jsx
+++ src/redux/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/contact">
-                Contact
+                Contacte
               </Link>
             </li>
             <li className="nav-item">
